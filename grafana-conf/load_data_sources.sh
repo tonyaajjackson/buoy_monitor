c25ba3e9d44c8cd4e3645452eb04b180187b97e9
Tony Jackson <tony.aaj.jackson@gmail.com>
2021-04-27 20:34:40 -0700

    Remove unnecessary wait-for in script
    wait-for is useful in starting docker containers in order, but is not
    necessary for a manually triggered script.

--- grafana-conf/load_data_sources.sh
+++ grafana-conf/load_data_sources.sh
@@ -4,13 +4,6 @@ set -a
 source ../.env
 set +a
 
-echo "Waiting for Grafana to come online"
-if ! ./wait-for grafana:3000; then
-    exit
-fi
-
-echo "Grafana is online"
-
 for i in data_sources/*; do
     curl -sS -X "POST" "http://grafana:3000/api/datasources" \
         -H "Content-Type: application/json" \
