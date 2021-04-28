e93b410335dfbb59eaef25fb8991e5454824c7f7
Tony Jackson <tony.aaj.jackson@gmail.com>
2021-04-27 23:24:11 -0700

    Move grafana to port 80 (no port)

--- grafana-conf/load_data_sources.sh
+++ grafana-conf/load_data_sources.sh
@@ -5,7 +5,7 @@ source ../.env
 set +a
 
 for i in data_sources/*; do
-    curl -sS -X "POST" "http://grafana:3000/api/datasources" \
+    curl -sS -X "POST" "http://grafana/api/datasources" \
         -H "Content-Type: application/json" \
         --user $GRAFANA_USERNAME:$GRAFANA_PASSWORD \
         --data-binary @$i
