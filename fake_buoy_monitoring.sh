b3bdeb9c220308c5946cfb9cb46b0f1e6ba71d40
Tony Jackson <tony.aaj.jackson@gmail.com>
2021-04-28 10:23:17 -0700

    Make buoy monitoring loop send data every 15s

--- fake_buoy_monitoring.sh
+++ fake_buoy_monitoring.sh
@@ -1,10 +1,14 @@
-for buoy_id in {0..9}; do
-    python ./monitor/monitor.py \
-        --graphite-host 127.0.0.1 \
-        --graphite-port 2004 \
-        --buoy-id buoy_${buoy_id} \
-        --lat 58.232067 \
-        --lon -13${buoy_id}
-    echo "Sending buoy_${buoy_id}"
-    sleep 1
+while true; do
+    date
+    for buoy_id in {0..9}; do
+        python ./monitor/monitor.py \
+            --graphite-host 127.0.0.1 \
+            --graphite-port 2004 \
+            --buoy-id buoy_${buoy_id} \
+            --lat 58.232067 \
+            --lon -13${buoy_id}
+        echo "Sending buoy_${buoy_id}"
+        sleep 1
+    done
+    sleep 5
 done
